ddaf5c509150f5a9e2abe18386691af51b830050
martin-ryunseok <martin@lightscale.io>
2024-10-01 07:23:44 +0900

    fix: output root 이벤트 토픽 업데이트

--- apps/indexer/lib/indexer/fetcher/optimism/output_root.ex
+++ apps/indexer/lib/indexer/fetcher/optimism/output_root.ex
@@ -22,7 +22,7 @@ defmodule Indexer.Fetcher.Optimism.OutputRoot do
   @stop_constant_key "optimism_output_roots_stopped"
 
   # 32-byte signature of the event OutputProposed(bytes32 indexed outputRoot, uint256 indexed l2OutputIndex, uint256 indexed l2BlockNumber, uint256 l1Timestamp)
-  @output_proposed_event "0xa7aaf2512769da4e444e3de247be2564225c2e7a8f74cfe528e46e17d24868e2"
+  @output_proposed_event "0x457b4388026260019ae0b0b4f16c98235d74fe7359be469bdcba16e6d0d49689"
 
   def child_spec(start_link_arguments) do
     spec = %{
